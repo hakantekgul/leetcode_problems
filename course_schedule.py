b96d1a851f9536c39e6c53db0bc13cdb19e5302d
hakantekgul <tekgul2@illinois.edu>
2019-11-15 19:21:35 +0100

    more problems

---
+++ course_schedule.py
@@ -0,0 +1,41 @@
+#### 207. Course Schedule ####
+'''
+There are a total of n courses you have to take, labeled from 0 to n-1.
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+Note:
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+'''
+# The question basically asks if the graph has a cycle. If it has one, the student can not finish all the courses. 
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        graph = [[] for _ in range(numCourses)]
+        visited = [0 for _ in range(numCourses)]
+        # create graph
+        for pair in prerequisites:
+            x, y = pair
+            graph[x].append(y)
+        # visit each node
+        for i in range(numCourses):
+            if not self.dfs(graph, visited, i):
+                return False
+        return True
+    
+    def dfs(self, graph, visited, i):
+        # if ith node is marked as being visited, then a cycle is found
+        if visited[i] == -1:
+            return False
+        # if it is done visted, then do not visit again
+        if visited[i] == 1:
+            return True
+        # mark as being visited
+        visited[i] = -1
+        # visit all the neighbours
+        for j in graph[i]:
+            if not self.dfs(graph, visited, j):
+                return False
+        # after visit all the neighbours, mark it as done visited
+        visited[i] = 1
+        return True
